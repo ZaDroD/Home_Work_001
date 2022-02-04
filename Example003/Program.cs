2ce773b611a289599ba0f70a6670a0b40f950cfa
Evgeniy <zadrod93@yandex.ru>
2022-02-04 21:03:29 +0300

    Задача номер 6

---
+++ Example003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число ");
+
+string number = Console.ReadLine();
+
+double num = int.Parse(number);
+
+if (num % 2 == 0) Console.WriteLine("Число чётноё " + num);
+if (num % 2 > 0) Console.WriteLine("Число не чётноё " + num);
