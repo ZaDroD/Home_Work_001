007b62c958864c584e5fbb55f18d7dfbbf8eb893
Evgeniy <zadrod93@yandex.ru>
2022-02-04 20:40:24 +0300

    Задача номер 4

---
+++ Example002/Program.cs
@@ -0,0 +1,17 @@
+﻿//Найти максимальное из трех чисел
+
+int numA = new Random().Next(1, 99);
+Console.WriteLine("Первое число " + numA);
+
+int numB = new Random().Next(1, 99);
+Console.WriteLine("Второе число " + numB);
+
+int numС = new Random().Next(1, 99);
+Console.WriteLine("Третье число " + numС);
+
+int max = numA;
+
+if ( max < numB) max = numB;
+if ( max < numС) max = numС;
+
+Console.WriteLine("Максимальное из трёх чисел " + max);
